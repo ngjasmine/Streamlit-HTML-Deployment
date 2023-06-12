175951d90ebd51ce4d1f4f67ef137d4ee7c90be1
ngjasmine <132629061+ngjasmine@users.noreply.github.com>
2023-06-12 11:56:00 +0800

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,13 @@ import streamlit.components.v1 as components
 # Specify html code below
 components.html(
     """
+    <style>
+    body {
+    background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fdata&psig=AOvVaw3u7cbEH1XjGNTkk1g-emvy&ust=1686627651460000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCIjO-OPnvP8CFQAAAAAdAAAAABAE');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    }
     <h1> My Portfolio </h1>
     <div> I am a junior data scientist specialising in Python and Machine Learning.</div>
     <hr />
